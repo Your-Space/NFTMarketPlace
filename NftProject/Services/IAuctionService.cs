fc15f6b4ef337491eda1fea260af683605ad0115
vtorkalo <torkalo@gmail.com>
2022-06-12 09:51:56 +0300

    nft debug builds

---
+++ NftProject/Services/IAuctionService.cs
@@ -0,0 +1,13 @@
+﻿using NftProject.Models;
+
+namespace NftProject.Services
+{
+    public interface IAuctionService
+    {
+        void CheckWinningAuctions();
+        IEnumerable<AuctionSaleModel> GetAllSalesByIdAsync(string TokenId);
+        Task SendMessage(AuctionInfoModel owner);
+        void SendMessage(AuctionInfoModel owner, AuctionSaleModel winner);
+        void SendMessage(string receiver, string subject, string message);
+    }
+}
